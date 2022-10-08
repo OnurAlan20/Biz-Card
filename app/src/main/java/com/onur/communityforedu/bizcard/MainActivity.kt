99af6c22c5ff2833eaf97c20e653aab1ccd5b358
OnurAlan20 <83537043+OnurAlan20@users.noreply.github.com>
2022-10-08 17:43:09 +0300

    Final Of BizCard

--- app/src/main/java/com/onur/communityforedu/bizcard/MainActivity.kt
+++ app/src/main/java/com/onur/communityforedu/bizcard/MainActivity.kt
@@ -5,18 +5,27 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.onur.communityforedu.bizcard.ui.theme.BizCardTheme
@@ -40,6 +49,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard(){
+    val buttonClickedState = remember {
+        mutableStateOf(false)
+    }
     Surface(modifier = Modifier
         .fillMaxHeight()
         .fillMaxWidth()) {
@@ -59,6 +71,20 @@ fun CreateBizCard(){
             ) {
                 CreateImageProfile()
                 Divider(thickness = 2.dp, color = Color.LightGray)
+                BizInfo()
+                Button(onClick = {
+                    buttonClickedState.value = !buttonClickedState.value
+                }) {
+                    Text(text = "Portfolio", style = MaterialTheme.typography.button)
+                }
+                if (buttonClickedState.value == true){
+                    Content()
+                }else{
+                    Box(){
+
+                    }
+                }
+
 
             }
         }
@@ -66,7 +92,6 @@ fun CreateBizCard(){
 
     }
 }
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
@@ -74,8 +99,8 @@ fun DefaultPreview() {
 }
 
 @Composable
-fun CreateImageProfile(){
-    Surface(modifier = Modifier
+fun CreateImageProfile(modifier: Modifier = Modifier){
+    Surface(modifier = modifier
         .size(150.dp)
         .padding(5.dp),
         shape = CircleShape,
@@ -84,8 +109,77 @@ fun CreateImageProfile(){
         color = MaterialTheme.colors.onSurface.copy(0.5f)
     ) {
         Image(painter = painterResource(id = R.drawable.pp), contentDescription = "profile image",
-            modifier = Modifier.size(135.dp),
+            modifier = modifier.size(135.dp),
             contentScale = ContentScale.Crop
         )
     }
 }
+
+@Composable
+private fun BizInfo() {
+    Column(modifier = Modifier.padding(5.dp)) {
+        Text(
+            text = "Onur Alan",
+            style = MaterialTheme.typography.h3,
+            color = MaterialTheme.colors.primaryVariant
+        )
+        Text(
+            text = "Android Developer",
+            modifier = Modifier.padding(3.dp),
+            style = MaterialTheme.typography.subtitle1
+        )
+        Text(
+            text = "@onuralan__",
+            modifier = Modifier.padding(3.dp),
+            style = MaterialTheme.typography.subtitle1
+        )
+    }
+}
+@Composable
+fun Content(){
+    Box(modifier = Modifier
+        .fillMaxHeight()
+        .fillMaxWidth()
+        .padding(5.dp)){
+        Surface(modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+            shape = RoundedCornerShape(corner = CornerSize(9.dp)),
+            border = BorderStroke(width = 2.dp, color = Color.LightGray)
+        ) {
+            Portfolio(data = listOf("Project1","Project2","project3","project4","project5"))
+        }
+    }
+}
+
+@Composable
+fun Portfolio(data: List<String>){
+    LazyColumn{
+        items(data){item->
+            Card(modifier = Modifier
+                .padding(13.dp)
+                .fillMaxWidth(), shape = RectangleShape, elevation = 4.dp) {
+                Row(modifier = Modifier
+                    .padding(8.dp)
+                    .background(MaterialTheme.colors.surface)
+                    .padding(16.dp), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Start) {
+                    CreateImageProfile(modifier = Modifier.size(100.dp))
+                    Column(
+                        Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight()
+                            .padding(9.dp), horizontalAlignment = Alignment.Start, verticalArrangement = Arrangement.Center) {
+                        Text(text = item, fontWeight = FontWeight.Bold, style = MaterialTheme.typography.button )
+                        Text("Onur Alan",)
+                        
+                    }
+                    
+                }
+                
+            }
+        }
+    }
+}
+
+
+
